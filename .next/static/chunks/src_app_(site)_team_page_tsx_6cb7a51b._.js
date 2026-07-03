88c0363e01d57eeda0db74ad9547a9495f845d33
Ronak <ronakdotasara05@gmail.com>
2026-07-03 14:46:29 +0530

    Use Next.js .gitignore; drop .next from version control

--- .next/static/chunks/src_app_(site)_team_page_tsx_6cb7a51b._.js
+++
@@ -1,7 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/_04461a82._.js"
-],
-    source: "dynamic"
-});
--- .next/static/chunks/src_app_layout_tsx_0a548d63._.js
+++
@@ -1,9 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/[root-of-the-server]__93027d07._.css",
-  "static/chunks/node_modules_84656cd9._.js",
-  "static/chunks/src_components_providers_tsx_a5078426._.js"
-],
-    source: "dynamic"
-});
